e9acd509fd90d995c915bb7459e15c1910e26566
Salvo Miosi <salvo.ilmiosi@gmail.com>
2017-02-15 12:53:05 +0100

    Added tinyfiledialogs (cross platform file dialog)

--- PinoMaze_maze/src/gui.cpp
+++ PinoMaze_maze/src/gui.cpp
@@ -1,87 +1,25 @@
 #include "gui.h"
 
-#ifdef _WIN32
+#include "tinyfiledialogs.h"
 
-#include <windows.h>
-#include <cstring>
+static const char *TITLE = "PinoMaze";
 
 const char *getMazeFilename(dialog_type diag, const char *lastName) {
-	static char filename[MAX_PATH];
-	memset(filename, 0, MAX_PATH);
-
-	if (lastName) {
-		snprintf(filename, MAX_PATH, "%s", lastName);
-	}
-
-	OPENFILENAME ofn;
-	ZeroMemory(&ofn, sizeof(ofn));
-
-	ofn.lStructSize = sizeof(ofn);
-	ofn.hwndOwner = nullptr;
-	ofn.lpstrFile = filename;
-	ofn.nMaxFile = MAX_PATH;
-	ofn.lpstrFilter = "Mazes (*.pmz)\0*.pmz\0All files (*.*)\0*.*\0";
-	ofn.nFilterIndex = 1;
-	ofn.lpstrFileTitle = nullptr;
-	ofn.nMaxFileTitle = 0;
-	ofn.lpstrInitialDir = nullptr;
-	ofn.Flags |= OFN_NOCHANGEDIR;
+	const char *FILTERS[] = {"*.pmz"};
+	const char *DESCRIPTION = "PinoMaze mazes";
 
 	switch (diag) {
 	case DIALOG_OPEN:
-	    if (GetOpenFileName(&ofn)) {
-	    	return filename;
-	    }
-	    break;
+		return tinyfd_openFileDialog(TITLE, "", 1, FILTERS, DESCRIPTION, 0);
 	case DIALOG_SAVE:
-		if (GetSaveFileName(&ofn)) {
-			return filename;
-		}
-		break;
+		return tinyfd_saveFileDialog(TITLE, "", 1, FILTERS, DESCRIPTION);
+	default:
+		return nullptr;
 	}
-
-    return nullptr;
 }
 
 void messageBox(message_type diag, const char *message) {
-	UINT flags = 0;
-	switch (diag) {
-	case MESSAGE_ERROR:
-		flags |= MB_OK | MB_ICONERROR;
-		break;
-	case MESSAGE_INFORMATION:
-		flags |= MB_OK | MB_ICONINFORMATION;
-		break;
-	case MESSAGE_WARNING:
-		flags |= MB_OK | MB_ICONWARNING;
-		break;
-	}
-	MessageBox(nullptr, message, "PinoMaze", flags);
+	const char *TYPES[] = {"info", "error", "warning"};
+
+	tinyfd_messageBox(TITLE, message, "ok", TYPES[static_cast<int>(diag)], 1);
 }
-
-#else
-
-#include <SDL2/SDL.h>
-
-const char *getMazeFilename(dialog_type diag, const char *lastName) {
-	// TODO
-	return nullptr;
-}
-
-void messageBox(message_type diag, const char *message) {
-	Uint32 flags = 0;
-	switch (diag) {
-	case MESSAGE_ERROR:
-		flags |= SDL_MESSAGEBOX_ERROR;
-		break;
-	case MESSAGE_INFORMATION:
-		flags |= SDL_MESSAGEBOX_INFORMATION;
-		break;
-	case MESSAGE_WARNING:
-		flags |= SDL_MESSAGEBOX_WARNING;
-		break;
-	}
-	SDL_ShowSimpleMessageBox(flags, "PinoMaze", message, nullptr);
-}
-
-#endif
