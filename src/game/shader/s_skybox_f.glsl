d32c7fe357e513111e1a64724310963c261ae0fd
Salvo Miosi <salvo.ilmiosi@gmail.com>
2023-08-28 09:26:44 +0200

    fix s_skybox_f sampler access

--- src/game/shader/s_skybox_f.glsl
+++ src/game/shader/s_skybox_f.glsl
@@ -8,5 +8,20 @@ out vec4 color;
 uniform sampler2D skyboxTexture[6];
 
 void main() {
-    color = texture(skyboxTexture[int(sampler_index)], texCoords);
+    switch (int(sampler_index)) {
+    case 0:
+        color = texture(skyboxTexture[0], texCoords); break;
+    case 1:
+        color = texture(skyboxTexture[1], texCoords); break;
+    case 2:
+        color = texture(skyboxTexture[2], texCoords); break;
+    case 3:
+        color = texture(skyboxTexture[3], texCoords); break;
+    case 4:
+        color = texture(skyboxTexture[4], texCoords); break;
+    case 5:
+        color = texture(skyboxTexture[5], texCoords); break;
+    default:
+        color = vec4(1.0);
+    }
 }
